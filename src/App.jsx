f574f701709e154f06d0146ef8a3c29914dce2d2
Shuaib Salam <shuaibsalam1234@gmail.com>
2023-11-30 21:19:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar/navbar"
+import HomePage from "./components/landingPAge/landin"
+
+function App() {
+
+  return (
+    <>
+     <Navbar/>
+     <HomePage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <div>
+wefwe
+    </div>
+  )
+}
+
+export default Navbar
